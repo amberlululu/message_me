dc5f833a7448f2dcfccedf02ec2405c52ee3b8a2
Amber Bai <bai.lu3838@gmail.com>
2020-04-30 17:38:43 -0400

    Add favicon

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # chatroom controller index action
+  root 'chatroom#index'
+  # session controller new action
+  get 'login', to: 'sessions#new'
 end
